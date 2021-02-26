8574ece2d2895d4a86a224aca51b64e048a6b70f
60635199 <60635199+maxreader@users.noreply.github.com>
2021-02-25 22:01:26 -0600

    linked new pages, renamed splash to index

--- main.py
+++ main.py
@@ -5,43 +5,58 @@ app = Flask(__name__)
 
 datafile = os.path.join(app.static_folder, 'data', 'data.json')
 data = json.load(open(datafile))
-artists = data["artists"]
-genres = data["genres"]
-albums = data["albums"]
+artistsData = data["artists"]
+genresData = data["genres"]
+albumsData = data["albums"]
 
 # genreIndex is used for dynamically updating navbar dropdown as genres are added
-genreIndex = [None]*len(genres)
-for thisGenre in genres:
+genreIndex = [None]*len(genresData)
+for thisGenre in genresData:
     genreIndex[int(thisGenre["id"])] = thisGenre
 
 
 def getArtistAlbums(albumList):
     artistAlbums = [None]*len(albumList)
     for i, albumID in enumerate(albumList):
-        artistAlbums[i] = albums[albumID]
+        artistAlbums[i] = albumsData[albumID]
     return artistAlbums
 
 
 @app.route('/')
 def index():
-    return render_template('splash.html', genreIndex=genreIndex)
+    return render_template('index.html', genreIndex=genreIndex)
+
+
+@app.route('/search-by-artist', methods=['GET'])
+def artists():
+    return render_template('artistsTable.html', genreIndex=genreIndex)
+
+
+@app.route('/search-by-song', methods=['GET'])
+def songs():
+    return render_template('songsTable.html', genreIndex=genreIndex)
+
+
+@app.route('/search-by-genre', methods=['GET'])
+def genres():
+    return render_template('genresTable.html', genreIndex=genreIndex)
 
 
 @app.route('/artist/<int:id>', methods=['GET'])
 def artist(id):
-    artistData = artists[int(id)]
+    artistData = artistsData[int(id)]
     artistAlbums = getArtistAlbums(artistData["albums"])
     return render_template('artistPage.html', artistData=artistData, artistAlbums=artistAlbums, genreIndex=genreIndex)
 
 
 @app.route('/genre/<int:id>', methods=['GET'])
 def genre(id):
-    return render_template('genrePage.html', genreData=genres[id], genreIndex=genreIndex)
+    return render_template('genrePage.html', genreData=genresData[id], genreIndex=genreIndex)
 
 
 @app.route('/album/<int:id>', methods=['GET'])
 def album(id):
-    return render_template('songPage.html', albumData=albums[id], genreIndex=genreIndex)
+    return render_template('songPage.html', albumData=albumsData[id], genreIndex=genreIndex)
 
 
 if __name__ == '__main__':
