35b0992f4f82774042b98965add3ea52d9dd7699
60635199 <60635199+maxreader@users.noreply.github.com>
2021-04-01 13:37:47 -0500

    Fixed relationship structure to artists-songs-albums instead of artists-albums-songs

--- models.py
+++ models.py
@@ -9,90 +9,90 @@ Base = declarative_base()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DB_STRING", 'postgres://postgres:asd123@localhost:5432/bookdb')
+    'DB_STRING', 'postgres://postgres:asd123@localhost:5432/bookdb')
 # to suppress a warning message
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-artist_album_table = Table('association', Base.metadata,
-                           Column("artist_id", Integer,
-                                  ForeignKey("artist.id")),
-                           Column("album_id", Integer,
-                                  ForeignKey("album.id"))
-                           )
-
-artist_genre_table = Table('association', Base.metadata,
-                           Column('artist_id', Integer,
-                                  ForeignKey('artist.id')),
-                           Column('genre_id', Integer, ForeignKey('artist.id'))
-                           )
-
-album_genre_table = Table('association', Base.metadata,
-                          Column('album_id', Integer,
-                                 ForeignKey('album.id')),
-                          Column('genre_id', Integer, ForeignKey('artist.id'))
+artist_song_table = Table('association', Base.metadata,
+                          Column('artist_id', Integer,
+                                 ForeignKey('artist.id')),
+                          Column('song_id', Integer,
+                                 ForeignKey('album.id'))
                           )
+
+song_album_table = Table('association', Base.metadata,
+                         Column('album_id', Integer,
+                                ForeignKey('artist.id')),
+                         Column('song_id', Integer,
+                                ForeignKey('album.id'))
+                         )
 
 
 class Artist(db.Model):
     # Artist has two primary attributes, ID and name
 
-    __tablename__ = "artist"
+    __tablename__ = 'artist'
 
     name = Column(db.String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+    songs = relationship(
+        'Song', secondary=artist_song_table, backpopulates="artists")
 
-    albums = relationship("Album",
-                          secondary=artist_album_table,
-                          backpopulates="artists")
+    albums = relationship("Album", lazy="joined", innerjoin=True)
 
-    genres = relationship("Genre",
+    """genres = relationship('Genre',
                           secondary=artist_genre_table,
-                          backpopulates="artists")
-
-
-class Album(db.Model):
-    # Album also has two primary attributes, ID and name
-
-    __tablename__ = "album"
-
-    name = Column(db.String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-
-    artists = relationship("Artist",
-                           secondary=artist_album_table,
-                           backpopulates="albums")
-
-    genres = relationship("Genre",
-                          secondary=artist_genre_table,
-                          backpopulates="albums")
+                          backpopulates='artists')"""
 
 
 class Song(db.Model):
     # Song is dependent on album, and can be identified by the position in that album
 
-    __tablename__ = "song"
+    __tablename__ = 'song'
+
+    title = Column(db.String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    album_id = Column(Integer, ForeignKey('album.id'))
+    artists = relationship(
+        'Artist', secondary=artist_song_table, backpopulates="songs")
+    albums = relationship(
+        'Album', secondary=song_album_table, backpopulates="songs")
+
+
+class Album(db.Model):
+    # Album also has two primary attributes, ID and name
+
+    __tablename__ = 'album'
+
+    title = Column(db.String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+    songs = relationship('Song',
+                         secondary=song_album_table,
+                         backpopulates='albums')
+
+    artists = relationship('Artist', lazy="joined", innerjoin=True)
+
+    """genres = relationship('Genre',
+                          secondary=artist_genre_table,
+                          backpopulates='albums')"""
+
+
+"""class Genre(db.Model):
+
+    __tablename__ = 'genre'
 
     name = Column(db.String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    album_id = Column(Integer, ForeignKey("album.id"))
-    album = relationship("Album", backref="songs")
 
-
-class Genre(db.Model):
-
-    __tablename__ = "genre"
-
-    name = Column(db.String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-
-    artists = relationship("Artist",
+    artists = relationship('Artist',
                            secondary=artist_album_table,
-                           backpopulates="genres")
+                           backpopulates='genres')
 
-    albums = relationship("Album",
+    albums = relationship('Album',
                           secondary=artist_album_table,
-                          backpopulates="genres")
+                          backpopulates='genres')"""
 
 
 db.drop_all()
